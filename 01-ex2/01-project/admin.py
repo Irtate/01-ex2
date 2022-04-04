493ec3ec44b46941139d8e749688f02ed07eafab
Irtate <100743016+Irtate@users.noreply.github.com>
2022-04-04 17:59:47 +0800

    Update admin.py

--- 01-ex2/01-project/admin.py
+++ 01-ex2/01-project/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from DP_Project.models.knapsack import Knapsack
 from DP_Project.models.logging import Logging
 
-admin.site.site_header = '项目管理后台'
+admin.site.site_header = '0-1KP实例算法实验平台'
 admin.site.site_title = '项目管理后台'
 admin.site.index_title = '项目管理后台'
 
